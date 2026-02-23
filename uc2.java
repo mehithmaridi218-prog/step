bbe1ac879dccb0c03ed8975448d685000d53ac18
mehithmaridi218-prog <mehithmaridi218@gmail.com>
2026-02-23 13:38:35 +0530

    HELLO

---
+++ uc2.java
@@ -0,0 +1,16 @@
+/**
+ * Use Case 2: ASCII Banner "OOPS"
+ * Constraints: 7-line grid, sequential print statements, manual alignment.
+ */
+public class uc2 {
+    public static void main(String[] args) {
+        // Each print statement represents one horizontal slice of the word "OOPS"
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** *** ");
+        System.out.println(" * * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
+    }
+}
