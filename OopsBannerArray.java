5914ef3323e8b22b9d066fd4cf8276076fd74e14
mehithmaridi218-prog <mehithmaridi218@gmail.com>
2026-02-24 15:38:46 +0530

    Printing OOPS using array init

--- OopsBannerArray.java
+++ OopsBannerArray.java
@@ -10,7 +10,7 @@ public class OopsBannerArray {
             String.join(" ", "  **  ", "   **  ", "  *     ", "**** ")
         };
 
-        // Use a for-each loop to iterate through the array and print each line
+        
         for (String line : lines) {
             System.out.println(line);
         }
